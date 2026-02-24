0d66178594c7d064d1c4d2e2ec4204494c1308b0
Michael Bohn <spaceodyssey@gmx.de>
2026-02-24 05:36:54 +0100

    add posts to patch

--- src/d_main.rs
+++ src/d_main.rs
@@ -12,10 +12,9 @@ pub fn do_advance_demo(game_state: &mut GameState) {
 }
 
 pub fn page_drawer(rdr: &mut RenderContext, game_state: &mut GameState) {
-    println!("### draw page: {}", game_state.page_name);
     let lump_ix =
         check_num_for_name(&game_state.lump_info, &game_state.page_name).expect("page lump");
     let patch = load_patch_lump(game_state, lump_ix).expect("page patch");
-    // TODO: Cache the patch lump, so it does not have to be constructed all the time?
+    // TODO: Cache the patch lump, so it does not have to be constructed all the time!
     draw_patch(rdr, 0, 0, &patch);
 }
--- src/v_patch.rs
+++ src/v_patch.rs
@@ -1,17 +1,23 @@
 use crate::{game::GameState, p_setup::load_lump, util::DataReader};
 
-pub struct Column {
-    /* TODO
-    Field 	Type 	Size 	Offset 	Description
-    topdelta 	uint8_t 	1 	0 	The y offset of this post in this patch. If 0xFF, then end-of-column (not a valid post)
-    length 	uint8_t 	1 	1 	Length of data in this post
-    ------unused 	uint8_t 	1 	2 	Unused padding byte; prevents error on column underflow due to loss of precision.
-    data 	uint8_t[] 	length 	3 	Array of pixels in this post; each data pixel is an index into the Doom palette.
-    -----unused 	uint8_t 	1 	3 + length 	Unused padding byte; prevents error on column overflow due to loss of precision.
-    */
+pub struct Post {
     pub top_delta: usize,
     pub length: usize,
     pub data: Vec<u8>,
+}
+
+pub struct Column {
+    pub posts: Vec<Post>,
+}
+
+impl Column {
+    fn new() -> Column {
+        Column { posts: Vec::new() }
+    }
+
+    fn add_post(&mut self, post: Post) {
+        self.posts.push(post);
+    }
 }
 
 pub struct Patch {
@@ -44,21 +50,31 @@ pub fn load_patch_lump(game_state: &mut GameState, lump: usize) -> Result<Patch,
     for i in 0..num_columns {
         let offset = column_ofs[i];
         reader.set_offset(offset as usize);
-        // read column
-        let top_delta = reader.read_u8() as usize;
-        let length = reader.read_u8() as usize;
-        reader.skip(1); //unused
-        let mut data = Vec::with_capacity(length);
-        for _ in 0..length {
-            data.push(reader.read_u8());
-        }
-        reader.skip(1); //unused
+        let mut column = Column::new();
 
-        columns.push(Column {
-            top_delta,
-            length,
-            data,
-        })
+        loop {
+            // read posts here and append them to the column
+            let top_delta = reader.read_u8() as usize;
+            if top_delta == 0xFF {
+                break;
+            }
+
+            let length = reader.read_u8() as usize;
+            reader.skip(1); //unused
+            let mut data = Vec::with_capacity(length);
+            for _ in 0..length {
+                data.push(reader.read_u8());
+            }
+            reader.skip(1); //unused
+
+            column.add_post(Post {
+                top_delta,
+                length,
+                data,
+            });
+        }
+
+        columns.push(column);
     }
 
     Ok(Patch {
